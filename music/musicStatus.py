fe273911b65757dc2d03b6355a25db8cb413e7f4
Pau Ruiz i Safont <unduthegun@gmail.com>
2014-04-13 20:37:42 +0200

    Corrected the status-fetching code in the music module

--- music/musicStatus.py
+++ music/musicStatus.py
@@ -1,6 +1,7 @@
 #coding:utf8
 from queryBot.responsemodule import IModule
 import lastfmapi
+import re
 
 api_key_g = 'api_key'
 service_g = 'service'
@@ -18,7 +19,7 @@ class MusicStatus(IModule):
         return True
 
     def get_command_patterns(self):
-        return [('^\.np (\S.*)', self.now_playing)]
+        return [('^\.np( (\S.*))?', self.now_playing)]
 
     def get_settings_name(self):
         return 'music'
@@ -44,44 +45,81 @@ class MusicStatus(IModule):
 
     # Functions that are needed for queries
     def now_playing(self, nick, message, channel):
-        result = self.fetch_status(nick)
-        #raise NotImplementedError()
+        # check the message to see if we're being asked for a user different
+        # from the user that sent the request
+        query = re.sub(r'^\.np', '', message.decode('utf-8'), re.UNICODE).encode('utf-8')
+        if query == u'':
+            user = nick
+        else:
+            user = query.strip(u' \t\n\r')
+
+        result = self.__fetch_status(user)
+
         if result and result != self.last_response:
             self.last_response = result
             return result.encode('utf-8', errors='ignore')
 
     def __fetch_status(self, luser):
-        #cleanup the message to get the query
-        #query = re.sub(r'^\.np', '', message.decode('utf-8'), re.UNICODE).encode('utf-8')
-        #luser = query.strip(u' \t\n\r')
         try:
             response = self.api.user_getRecentTracks(user=luser.encode('utf-8'))
         except lastfmapi.LastFmApiException:
             return u' '.join([luser, u'doesn\'t exist'])
+
+        # if we later cannot find a song, we already know why:
         message = u' '.join([luser, u'isn\'t playing any song'])
 
         if u'track' in response[u'recenttracks'].keys() and len(response[u'recenttracks'][u'track']) > 0:
             rtrack = response[u'recenttracks'][u'track'][0]
-            if u'@attr' in rtrack.keys() and u'nowplaying' in rtrack['@attr'].keys():
+
+            print (rtrack)
+            play_text = u'last played'
+            time      = u''
+            if u'@attr' in rtrack.keys() and u'nowplaying' in rtrack[u'@attr'].keys():
+                play_text = u'is now playing'
+            elif u'date' in rtrack.keys() and u'#text' in rtrack[u'date'].keys():
+                time = u' on ' + rtrack[u'date'][u'#text']
+
+            if u'name' in rtrack.keys() and \
+                 u'artist' in rtrack.keys() and u'#text' in rtrack[u'artist'].keys():
                 ltitle = rtrack[u'name']
                 lartist = rtrack[u'artist'][u'#text']
-
-                message = u' '.join([luser,  u'is now playing', ltitle, u'—',  lartist])
-
-            try:
-                response = self.api.track_getInfo(track=ltitle.encode('utf-8'),artist=lartist.encode('utf-8'),username=luser.encode('utf-8'))
-            except lastfmapi.LastFmApiException:
+            else:
+                # we don't have title nor artist!!!
                 return message
 
-            if u'userloved' in response[u'track'].keys() and response[u'track'][u'userloved'] == '1':
-                message += ' ♥'
+            loved    = u''
+            tag_list = u''
+
             try:
-                toptags = response[u'track'][u'toptags'][u'tag']
-                message += ' ('
-                for tag in toptags:
-                    message += tag[u'name'] + ', '
-                message = message[:-2] + ')'
-            except Exception:
+                #fetch more info avout the track to display more info about it
+                response = self.api.track_getInfo(track=ltitle.encode('utf-8'),
+                                                  artist=lartist.encode('utf-8'),
+                                                  username=luser.encode('utf-8'))
+
+                # see if it's a 'loved' track
+                if u'userloved' in response[u'track'].keys() and response[u'track'][u'userloved'] == '1':
+                    loved = ' ♥'
+
+                # add the tags of the track
+                try:
+                    toptags = response[u'track'][u'toptags'][u'tag']
+                    tag_list = ' ('
+                    for tag in toptags:
+                        tag_list += tag[u'name'] + ', '
+                    tag_list = tag_list[:-2] + ')'
+                except Exception:
+                    pass
+
+            except lastfmapi.LastFmApiException:
                 pass
+
+            message = u'{user} {done}: {song} — {artist}{heart}{tags}{when}'.format(
+                user   = luser,
+                done   = play_text,
+                song   = ltitle,
+                artist = lartist,
+                heart  = loved,
+                tags   = tag_list,
+                when   = time)
 
         return message
